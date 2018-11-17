9a56b673c50e3040cec1e5cacfa46a2a6c2b8e06
Midkey <huang6469@qq.com>
2018-11-18 00:35:14 +0800

    first comment

--- main2.py
+++ main2.py
@@ -1,14 +1,18 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-im = cv2.imread('8.png')
-# im = cv2.imread('6.png')
+im_name = '5.jpg'
+im_path = 'sample'
+save_path = 'output'
+im = cv2.imread(os.path.join(im_path, im_name))
 im = np.asarray(im, dtype=np.float)
 patch_size = (15 - 1) // 2
 t0 = 0.1
+omega = 0.9
+dark_channel_maximum = 220
 height, width, channel = np.shape(im)
-
 
 j_dark = np.zeros((height, width))
 for i in range(height):
@@ -18,17 +22,10 @@ for i in range(height):
 dark_channel_list = np.sort(np.reshape(j_dark, (-1, 1)))
 dark_channel_max = dark_channel_list[int(len(dark_channel_list) * 0.001)]
 A = np.max(im[j_dark>dark_channel_max, :], axis=0)
+A[A > dark_channel_maximum] = dark_channel_maximum
 
-j_map = np.zeros((height, width, 3))
 
 t_ = np.zeros((height, width))
-for i in range(height):
-    for j in range(width):
-        # patch = im[max(i - patch_size, 0):min(i + patch_size, height), max(j - patch_size, 0):min(j + patch_size, width), :]
-        # t_[i, j] = 1 - 0.95 * np.min(patch/A)
-        t_[i, j] = 1 - 0.8 * np.min(im[i, j, :]/A)
-
-
 L = np.zeros((height, width))
 for i in range(height):
     for j in range(width):
@@ -47,30 +44,39 @@ for i in range(height):
         V = (delta + epsilon/wk*U3).I
         L[i, j] = 1 - 1 - 1 / wk * np.trace(Ii * V * Ij.T)
 
+        # calculate the t~
+        # patch = im[max(i - patch_size, 0):min(i + patch_size, height), max(j - patch_size, 0):min(j + patch_size, width), :]
+        # t_[i, j] = 1 - 0.95 * np.min(patch/A)
+        t_[i, j] = 1 - omega * np.min(im[i, j, :] / A)
 
-lambda_ = 0.0001
-U = np.zeros((height, width))
+
+lambda_ = 0.001
+U = np.ones((height, width))
 m = min(width, height)
-U[0:m, 0:m] = np.eye(m)
-t = np.mat(L + lambda_ * U).I * lambda_ * t_
+# U[0:m, 0:m] = np.eye(m)
+# t = np.mat(L + lambda_ * U).I * lambda_ * t_  # something error
+t = (L + lambda_ * U)**(-1) * lambda_ * t_  # something error
+
 # t = (L + lambda_ * U) ** (-1) * lambda_ * t_
 # t = np.zeros((height, width))
-for i in range(height):
-    for j in range(width):
-        j_map[i, j, :] = (im[i, j, :] - A) / max(t[i, j], t0) + A
+# j_map = np.zeros((height, width, 3))
 
 
-# plt.subplot(121)
-# plt.imshow(im)
-# plt.subplot(122)
-# plt.imshow(j_dark)
-# plt.show()
-# A = np.max()
+t = np.max((t, np.ones(np.shape(t))*t0), axis=0)[:, :, np.newaxis]
+t = np.tile(t, (1, 1, 3))
+A = np.reshape(A, (1, 1, 3))
+A = np.tile(A, (height, width, 1))
+j_map = (im - A) / t + A
+j_map[j_map>255] = 255
+j_map[j_map<0] = 0
+# for i in range(height):
+#     for j in range(width):
+#         j_map[i, j, :] = (im[i, j, :] - A) / max(t[i, j], t0) + A
+
 cv2.imshow(u'raw image', np.asarray(im, dtype=np.uint8))
 cv2.imshow(u'result', np.asarray(j_map, dtype=np.uint8))
 cv2.imshow(u'dark channel', np.asarray(j_dark, dtype=np.uint8))
 cv2.imshow(u'transmission', np.asarray(t*255, dtype=np.uint8))
 
-# plt.imshow(j_dark)
-# plt.show()
+cv2.imwrite(os.path.join(save_path, im_name), np.asarray(j_map, dtype=np.uint8))
 cv2.waitKey(0)
