c0b84184c340ae44cbb9c25d9e8a0fbb7a8acc23
Midkey <huang6469@qq.com>
2018-11-17 21:23:49 +0800

    first comment

--- main3.py
+++ main3.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-im = cv2.imread('8.png')
-# im = cv2.imread('6.png')
+im = cv2.imread('2.jpg')
+# im = cv2.imread('3.png')
 im = np.asarray(im, dtype=np.float)
-patch_size = (15 - 1) // 2
+patch_size = (7 - 1) // 2
 t0 = 0.1
 height, width, channel = np.shape(im)
 
@@ -24,15 +24,16 @@ j_map = np.zeros((height, width, 3))
 t_ = np.zeros((height, width))
 for i in range(height):
     for j in range(width):
-        # patch = im[max(i - patch_size, 0):min(i + patch_size, height), max(j - patch_size, 0):min(j + patch_size, width), :]
-        # t_[i, j] = 1 - 0.95 * np.min(patch/A)
-        t_[i, j] = 1 - 0.8 * np.min(im[i, j, :]/A)
+        patch = im[max(i - patch_size, 0):min(i + patch_size, height), max(j - patch_size, 0):min(j + patch_size, width), :]
+        t_[i, j] = 1 - 0.95 * np.min(patch/A)
+        # t_[i, j] = 1 - 0.95 * np.min(im[i, j, :]/A)
 
 
+lambda_ = 0.0001
 L = np.zeros((height, width))
+t = np.zeros((height, width))
 for i in range(height):
     for j in range(width):
-        l = 0
         center = im[i, j]
         patch = im[max(i - patch_size, 0):min(i + patch_size, height), max(j - patch_size, 0):min(j + patch_size, width), :]
         patch = np.reshape(patch, (-1, 3))
@@ -46,13 +47,13 @@ for i in range(height):
         U3 = np.mat(np.eye(3))
         V = (delta + epsilon/wk*U3).I
         L[i, j] = 1 - 1 - 1 / wk * np.trace(Ii * V * Ij.T)
+        l = L[i, j]
+        t[i, j] = (l + lambda_)**(-1) * t_[i, j]
 
-
-lambda_ = 0.0001
-U = np.zeros((height, width))
-m = min(width, height)
-U[0:m, 0:m] = np.eye(m)
-t = np.mat(L + lambda_ * U).I * lambda_ * t_
+# U = np.zeros((height, width))
+# m = min(width, height)
+# U[0:m, 0:m] = np.eye(m)
+# t = (L + lambda_ * U).I * lambda_ * t_
 # t = (L + lambda_ * U) ** (-1) * lambda_ * t_
 # t = np.zeros((height, width))
 for i in range(height):
